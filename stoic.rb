9f4edeb8cbb930b6c9dfe05d093c238db676c28a
Stanislav Katkov <contact@skatkov.com>
2022-05-30 01:44:44 +0200

    adding stoic library

---
+++ stoic.rb
@@ -0,0 +1,20 @@
+class Stoic < Formula
+  desc "Command-line app for daily journaling with plain-text files"
+  homepage "https://github.com/skatkov/stoic"
+  url "https://github.com/skatkov/stoic/archive/refs/tags/0.2.1.tar.gz"
+  version "0.2.1"
+  sha256 "d27e9689af3fcf613ea64ab35f669a5144274b03a691516a90aad71c3d273d88"
+  license "MIT"
+  head "https://github.com/skatkov/stoic.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+      system "go", "build", *std_go_args
+  end
+
+  test do
+      system "#{bin}/stoic", '-about'
+  end
+end
+  
