54b24f53df0d82eaa17f6f92c9083a54de85ec7f
Stanislav (Stas) Katkov <sk@skylup.com>
2022-06-12 20:38:52 +0200

    Update stoic.rb

--- stoic.rb
+++ stoic.rb
@@ -3,7 +3,7 @@ class Stoic < Formula
   homepage "https://github.com/skatkov/stoic"
   url "https://github.com/skatkov/stoic/archive/refs/tags/0.3.1.tar.gz"
   version "0.3.1"
-  sha256 "e187f04209f3b7bcc678f6bc0aafe825447c264468d95170066e23ee1b08f708"
+  #sha256 "e187f04209f3b7bcc678f6bc0aafe825447c264468d95170066e23ee1b08f708"
   license "MIT"
   head "https://github.com/skatkov/stoic.git", branch: "main"
 
