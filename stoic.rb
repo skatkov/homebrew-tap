3e583e9d070b1ee8be7cde53ad6a5efc49ec66eb
Stanislav Katkov <contact@skatkov.com>
2022-06-12 20:02:46 +0200

    update stoic

--- stoic.rb
+++ stoic.rb
@@ -1,9 +1,9 @@
 class Stoic < Formula
   desc "Command-line app for daily journaling with plain-text files"
   homepage "https://github.com/skatkov/stoic"
-  url "https://github.com/skatkov/stoic/archive/refs/tags/0.2.1.tar.gz"
-  version "0.2.1"
-  sha256 "d27e9689af3fcf613ea64ab35f669a5144274b03a691516a90aad71c3d273d88"
+  url "https://github.com/skatkov/stoic/archive/refs/tags/0.3.tar.gz"
+  version "0.3"
+  # sha256 "d27e9689af3fcf613ea64ab35f669a5144274b03a691516a90aad71c3d273d88"
   license "MIT"
   head "https://github.com/skatkov/stoic.git", branch: "main"
 
